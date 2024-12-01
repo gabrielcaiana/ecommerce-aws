4e0e3232e9d2bc04b4eadf8180512ef01522db7c
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-12-01 16:29:47 -0300

    Adiciona stack do API Ecommerce com integração Lambda e configuração de logs

---
+++ lib/ecommerceApi-stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as cwlogs from 'aws-cdk-lib/aws-logs';
+
+import { Construct } from 'constructs';
+
+interface EcommerceApiStackProps extends cdk.StackProps {
+  readonly productsHandler: lambdaNodejs.NodejsFunction;
+}
+
+export class EcommerceApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
+    super(scope, id, props);
+
+    const logGroup = new cwlogs.LogGroup(this, 'EcommerceApiLogs')
+    
+    const api = new apigateway.RestApi(this, 'EcommerceApi', {
+      restApiName: 'EcommerceApi',
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          caller: true,
+          httpMethod: true,
+          ip: false,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          user: false
+        })
+      }
+    })
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsHandler);
+
+    // /products
+    const productsResource = api.root.addResource('products');
+    productsResource.addMethod('GET', productsFetchIntegration);
+  }
+}
+
